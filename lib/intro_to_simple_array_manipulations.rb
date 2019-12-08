6ac7c6880365225212e20aa898302251a8e4f1fe
Zach Weber <weberzt@gmail.com>
2019-12-08 23:37:51 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(array, string)
+   array.push(string)
+ end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array, n = 2)
+  array.pop(n)
+end
+  
+def using_shift(array)
+  array.shift 
+end
+  
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, i)
+  array.delete_at(i)
+end
+def shift_with_args(array, n = 2)
+  array.shift(n)
+end
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert(array, element, i = 4)
+   array.insert(i, element)
+ end
+ 
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten 
+end
+
